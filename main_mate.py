22b2f023e56173fa15758a5f292549e8e14fa240
Muskan <imh10017.18@bitmesra.ac.in>
2021-04-09 10:32:15 +0530

    Added an email feature to Mate

--- main_mate.py
+++ main_mate.py
@@ -4,17 +4,18 @@ import speech_recognition as sr
 import wikipedia
 import webbrowser
 import os
-
+import smtplib
+import random
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
 
 
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
 
 def wishMe():
     '''Add doc string'''
@@ -29,6 +30,7 @@ def wishMe():
         speak("Wishing you a very wonderful evening.")
 
     speak("Hi! I'm your Mate . How may I assist you?")
+
 
 def takeCommand():
     '''Add doc string'''
@@ -49,43 +51,66 @@ def takeCommand():
     return query
 
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your_password')
+    server.sendmail('email@gmail.com', to, content)
+    server.close()
+
 
 if __name__ == '__main__':
-  wishMe()
-  while True:
-      query = takeCommand().lower()
+    wishMe()
+    while True:
+        query = takeCommand().lower()
 
-      if 'wikipedia' in query:
-          speak("Searching Wikipedia...")
-          query = query.replace("wikipedia","")
-          results = wikipedia.summary(query,sentences=2)
-          speak("According to wikipedia")
-          print(results)
-          speak(results)
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
 
-      elif 'open youtube' in query:
-          webbrowser.open("youtube.com")
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
 
-      elif 'open google' in query:
-          webbrowser.open("google.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
 
-      elif 'open kaggle' in query:
-          webbrowser.open("kaggle.com")
+        elif 'open kaggle' in query:
+            webbrowser.open("kaggle.com")
 
-      elif 'open gmail' in query:
-          webbrowser.open("gmail.com")
+        elif 'open gmail' in query:
+            webbrowser.open("gmail.com")
 
-      elif 'play music' in query:
-          music_dir = 'C:\\Users\\mini9\\Desktop\\Music'
-          songs = os.listdir(music_dir)
-          print(songs)
-          os.startfile(os.path.join(music_dir,songs[0]))
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\mini9\\Desktop\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+            os.startfile(os.path.join(music_dir, random.choice(songs)))
 
-      elif 'the time' in query:
-          strTime = datetime.datetime.now().strftime("%H:%M:%S")
-          speak(f"The time is {strTime}")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {strTime}")
 
-      elif 'open VisualStudio' in query:
-          codepath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\devenv.exe"
-          os.startfile(codepath)
-          
+        elif 'open VisualStudio' in query:
+            codepath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\devenv.exe"
+            os.startfile(codepath)
+
+        elif 'email to mini' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "email@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent")
+            except Exception as e:
+                print(e)
+                speak("Sorry, I was unable to send the Email. Try again please")
+
+        elif 'switch off' in query:
+            speak("Glad I helped you, Bye")
+            exit()
